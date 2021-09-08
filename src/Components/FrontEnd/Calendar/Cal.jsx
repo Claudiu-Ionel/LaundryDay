10b85fc42096b90454bbdedc7bf43e7f647c4874
Ålexander <rockysfairat@gmail.com>
2021-09-08 14:24:01 +0200

    tweaked the Calendar a bit

--- src/Components/FrontEnd/Calendar/Cal.jsx
+++ src/Components/FrontEnd/Calendar/Cal.jsx
@@ -4,6 +4,11 @@ import Calendar from "react-calendar";
 import "./cal.css";
 
 function Cal() {
+  let test = document.querySelector(".react-calendar__month-view__days__day");
+
+  console.log(test);
+
+  const [weekNumber, setWeekNumber] = useState(new Date());
   return (
     <div className="calendar-container">
       <Calendar />
